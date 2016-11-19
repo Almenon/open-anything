1394df5267282c03bffcef28890e76e15c3d36dc
Almenon <caleb.collinsparks@gmail.com>
2016-11-19 08:23:54 -0800

    added xml support and handling for MemoryError

--- quickOpen.py
+++ quickOpen.py
@@ -5,19 +5,40 @@ from os import path
 
 protocols = ['https://','http://','www.']
 
-def qopen(name):
+def qopen(name,size=None):
+    try:
+        fileName, fileType = path.splitext(name)
+        if(fileType == '.csv'):
+            return openDelimitedFile(name, ',', size)
+        elif(fileType == '.tsv'):
+            return openDelimitedFile(name, '\t', size)
+        elif(fileType == '.json'):
+            return openJson(name)
+        elif(fileType == '.lnk'):
+            return openLnk(name)
+        elif(fileType == '.xml'):
+            return openXml(name)
+        elif(any([protocol in name for protocol in protocols])):
+            openWebsite(name)
+        else:
+            return openTextFile(name, size)
+    except MemoryError as e:
+        print("Memory Error - can't load in entire file.")
+        if(fileType not in ['.json','.xml']):
+            print('Loading in first 2048 bytes instead')
+            qopen(name,2048)
+        else: print(e)
 
-    fileName, fileType = path.splitext(name)
-    if(fileType == '.csv'):
-        return openDelimitedFile(name,',')
-    elif(fileType == '.tsv'):
-        return openDelimitedFile(name,'\t')
-    elif(fileType == '.json'):
-        return openJson(name)
-    elif(any([protocol in name for protocol in protocols])):
-        openWebsite(name)
-    else:
-        return openTextFile(name)
+
+def openXml(name):
+    module = import_module('xml.etree.ElementTree')
+    parse = getattr(module, 'parse')
+    return parse(name)
+
+def openLnk(name):
+    print('lnk files not supported yet\n'
+          'try installing https://pypi.python.org/pypi/pylnk/')
+
 
 def openJson(name):
     module = import_module('json')
@@ -35,11 +56,12 @@ def openWebsite(url):
         urlopen = getattr(module, 'urlopen')
         return urlopen(url)
 
-def openTextFile(name):
+def openTextFile(name, size):
     with open(name) as f:
-        return f.read()
+        return f.read(size)
 
-def openDelimitedFile(name,delimiter):
+def openDelimitedFile(name,delimiter, size):
+    if(size is not None): print('size param not supported yet')
     try:
         module = import_module('pandas')
         read_csv = getattr(module,'read_csv')
@@ -52,8 +74,8 @@ def openDelimitedFile(name,delimiter):
             return [line for line in csvReader]
 
 
-#result = qopen('temp.txt')
-#print(result)
+# result = qopen('testFiles\\t.xml')
+# print(result)
 
 # links::
 # http://stackoverflow.com/questions/12136850/tab-delimited-file-using-csv-reader-not-delimiting-where-i-expect-it-to
